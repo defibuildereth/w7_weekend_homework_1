8723087a4fe0ff49dc49f10fd25681ee5607b34e
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-03 14:38:58 +0100

    getting api resonse to console

--- src/components/Response.js
+++ src/components/Response.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const Response = () => {
+const Response = ({info}) => {
+
+    console.log(info)
+
     return (
         <h4>I'm the response</h4>
     )
--- src/containers/EtherscanContainer.js
+++ src/containers/EtherscanContainer.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Response from '../components/Response'
 
 const EtherscanContainer = () => {
+
+    const [info, setInfo] = useState([]);
+
+    useEffect(() => {
+        getInfo();
+    }, []);
+
+    const getInfo = function(){
+        fetch('https://api.etherscan.io/api?module=account&action=tokennfttx&&address=0x23b50b9815eedd4e2de9227aa462144b548c34cd&page=1&offset=100&sort=asc&apikey=IZKIMPDE58Y59KW2RSD9NDYYY39HSBY1QJ')
+        .then(res => res.json())
+        .then(munros => setInfo(munros))
+    }
+
+    // getInfo()
+
     return (
         <>
         <h2>I'm the EtherscanContainer</h2>
-        <Response/>
+        <Response info={info}/>
         </>
     )
 }
