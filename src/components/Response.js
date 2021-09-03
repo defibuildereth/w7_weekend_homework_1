eb2180f3503a948896faba2fcd36185deb69e5cc
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-03 14:06:36 +0100

    App renders container renders component

---
+++ src/components/Response.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Response = () => {
+    return (
+        <h4>I'm the response</h4>
+    )
+}
+
+
+export default Response
---
+++ src/containers/EtherscanContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Response from '../components/Response'
+
+const EtherscanContainer = () => {
+    return (
+        <>
+        <h2>I'm the EtherscanContainer</h2>
+        <Response/>
+        </>
+    )
+}
+
+
+export default EtherscanContainer
