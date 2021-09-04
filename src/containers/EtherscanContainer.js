ea3c8002d2f7e6710ea33cc7de159a09660be727
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-04 12:07:46 +0100

    re-thought layout

--- src/containers/EtherscanContainer.js
+++ src/containers/EtherscanContainer.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
-import Response from '../components/Response'
+import TransactionsList from '../components/TransactionsList';
+
 
 const EtherscanContainer = () => {
 
@@ -11,16 +12,19 @@ const EtherscanContainer = () => {
 
     const getInfo = function(){
         fetch('https://api.etherscan.io/api?module=account&action=tokennfttx&&address=0x23b50b9815eedd4e2de9227aa462144b548c34cd&page=1&offset=100&sort=asc&apikey=IZKIMPDE58Y59KW2RSD9NDYYY39HSBY1QJ')
-        .then(res => res.json())
-        .then(munros => setInfo(munros))
+        .then((res) => res.json())
+        .then(info => setInfo(info));
+        
+        // console.log(info);
+        
     }
 
-    // getInfo()
+    
 
     return (
         <>
         <h2>I'm the EtherscanContainer</h2>
-        <Response info={info}/>
+        <TransactionsList info={info}/>
         </>
     )
 }
